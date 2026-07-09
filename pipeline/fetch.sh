4ea81798d922efddc52d6521129296880bb5023b
Alec <alecsharpie@gmail.com>
2026-07-09 17:44:17 +1000

    Revert "Site consistency pass: unify header nav, footer tagline, chrome tokens"
    This reverts commit f588ffa47e6c835fe8e0d516ea418a726e44cc17.

--- pipeline/fetch.sh
+++ pipeline/fetch.sh
@@ -8,9 +8,7 @@ cd "$(dirname "$0")/../data/raw"
 # 2388  Bhagavad Gita (Arnold)        3330  Analects (Legge)
 # 45109 Enchiridion (Higginson)       3434  Koran (Rodwell)
 # 3283  Upanishads (Paramananda)      57342 Diogenes Laertius (Yonge; Epicurus)
-# 59709 Zhuangzi (Giles)              10056 Sayings of Mencius (Giles anthology)
-# 8547  Pirkei Avot (Taylor)          45159 Rumi, The Persian Mystics (Davis)
-for id in 216 2017 2680 10 2388 3330 45109 3434 3283 57342 59709 10056 8547 45159; do
+for id in 216 2017 2680 10 2388 3330 45109 3434 3283 57342; do
   [ -f "pg${id}.txt" ] || curl -sL -o "pg${id}.txt" "https://www.gutenberg.org/cache/epub/${id}/pg${id}.txt" &
 done
 wait
